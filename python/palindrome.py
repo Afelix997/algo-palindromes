83d1084a71e15520e72dd53cc218e087e72f68fc
afelix997 <afelix2013@gmail.com>
2022-05-29 20:10:18 -0700

    answer

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,7 @@
 def palindrome(word):
-    # Write code here
+    word = (str(word)).lower()
+    word = "".join(filter(str.isalnum, word))
+    rev_word= word[::-1]
+    return word == rev_word
+
+
