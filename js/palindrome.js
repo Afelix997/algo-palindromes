bc53b77aff685791db3caf6b8161c41221580c56
afelix997 <afelix2013@gmail.com>
2022-05-29 19:56:28 -0700

    answer

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,7 @@
 exports.palindrome = function(word) {
+    word= word.toString().replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
+    let revWord= word.split("").reverse().join("")
+    return (word === revWord) 
+}
 
-};
+
